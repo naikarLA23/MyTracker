12b995b5df1d84dd63a965dadb72f141859a540d
L A Naikar <70810522+naikarLA23@users.noreply.github.com>
2025-01-18 19:25:52 +0530

    Delete MyTracker/MyTracker directory

--- MyTracker/MyTracker/Helper/Constants/AppSettingKeys.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MyTracker.Helper.Constants
-{
-    public class AppSettingKeys
-    {
-        public const string JWT_ISSUER = "Jwt:Issuer";
-        public const string JWT_EXPIRY_MINUTES = "Jwt:ExpiryMinutes";
-        public const string JWT_Key = "Jwt:Key";
-    }
-}
--- MyTracker/MyTracker/Models/Enums/ResponseStatus.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MyTracker.Models.Enums
-{
-    public enum ResponseStatus
-    {
-        Success = 0,
-        Warning = 1,
-        Error = 2
-    }
-}
